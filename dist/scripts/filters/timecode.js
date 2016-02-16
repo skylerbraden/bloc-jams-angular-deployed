4df76ef38472896a2d3784bf8bebdd9ccf7c5324
Skyler Braden <skylerbraden@me.com>
2016-02-16 12:14:22 -0600

    Assignment 11, refactored timecode using Buzz toTimer() method

--- dist/scripts/filters/timecode.js
+++ dist/scripts/filters/timecode.js
@@ -1,23 +1,31 @@
 (function() {
+//    function timecode() {
+//        return function(seconds) {
+//            var seconds = Number.parseFloat(seconds);
+//            if (Number.isNaN(seconds)) {
+//                return '-:--';
+//            }
+//            var wholeSeconds = Math.floor(seconds);
+//            var minutes = Math.floor(wholeSeconds / 60);
+//            var remainingSeconds = wholeSeconds % 60;
+//            
+//            var output = minutes + ":";
+//            
+//            if (remainingSeconds < 10) {
+//                output += '0';
+//            }
+//            
+//            output += remainingSeconds;
+//            
+//            return output;
+//        };
+//    }
+    
     function timecode() {
         return function(seconds) {
             var seconds = Number.parseFloat(seconds);
-            if (Number.isNaN(seconds)) {
-                return '-:--';
-            }
-            var wholeSeconds = Math.floor(seconds);
-            var minutes = Math.floor(wholeSeconds / 60);
-            var remainingSeconds = wholeSeconds % 60;
-            
-            var output = minutes + ":";
-            
-            if (remainingSeconds < 10) {
-                output += '0';
-            }
-            
-            output += remainingSeconds;
-            
-            return output;
+            var timer = buzz.toTimer(seconds);
+            return timer;
         };
     }
     
